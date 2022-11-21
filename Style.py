3fad8e2582cb14371715bd07ca52305f78e65be7
Xtarii <56205541+Xtarii@users.noreply.github.com>
2022-11-21 10:48:27 +0100

    New Functions
    Added a new loadingbar:
    "Loading: [#####         ] 50%
    
    Added documentation to the functons so it is easier to understand what they do

--- Style.py
+++ Style.py
@@ -1,12 +1,10 @@
-#this file was created by Xtarii and uploaded to Github: https://github.com/Xtarii/PythonStylePack
-
 #imports:
 import time
 
 
 
 #extras:
-#this part is taken form google "StackOverflow" https://stackoverflow.com/questions/67241111/python-colored-text-to-the-terminal#:~:text=The%20most%20basic%20terminals%20have%20a%20set%20of,%5Cu001b%20%5B36m%208%20White%3A%20%5Cu001b%20%5B37m%20Fler%20objekt
+#this part is googled becuse colors is complicated
 Colors = {
     "HEADER": '\033[95m',
     "BLUE": '\033[94m',
@@ -22,8 +20,18 @@ Colors = {
 
 
 #defs:
-#this is a round loadingbar
+#this is a spinning loadingbar
 def loadingbar_spinning(sleep=float(0.1), times=int(3), text=str("Loading: ")):
+    """
+    This is a loadingbar animation of a spinnign wheel
+
+    sleep:  time delay before next character is printed
+    times:  how many times the circle should spin around
+    text:   set the text before loading
+
+    ends with two new lines, and the default color
+    """
+
     print(Colors["BOLD"] + text + " ", sep="", end="", flush=True)
     for x in range(times):
         for x in r"-\|/-\|/":
@@ -35,9 +43,49 @@ def loadingbar_spinning(sleep=float(0.1), times=int(3), text=str("Loading: ")):
 
 #this is a loadingbar line
 def loadingbar_line(sleep=float(0.1), times=int(51), text=str("Loading: ")):
+    """
+    This is a animation of a loadingbar line: ############
+
+    sleep:  time delay before next character is printed
+    times:  how many times to print "#"
+    text:   set the text before loading
+
+    ends with two new lines, and the default color
+    """
+
     print(Colors["BOLD"] + text + " ", sep="", end="", flush=True)
     for x in range(times):
         print(Colors["FAIL"] + "#", sep="", end="", flush=True)
         time.sleep(sleep)
     
     print(Colors["ENDC"] + "\n\n")
+
+
+#this is a other loadingbar line
+def loadLineDL(sleep=float(0.1), text=str("Loading:")):
+    """
+    This loadingbar will look something like this:
+    Loading: [#####     ] 50%
+
+    sleep:  time delay before next character is printed
+    text:   set the text before loading, by default it has not space
+
+    this time you can not change amount of characters to print
+    it will always print "##" 10 times: ####################
+
+    ends with two new lines, and the default color
+    """
+
+    t = ""
+    space = "                    "
+    for x in range(10):
+        procent = x * 10
+        t = t + "##"
+        space = (space + "\b\b")
+        line = (t + space)
+
+        print(Colors["BOLD"] + text, Colors["FAIL"] + f"[{line}]", Colors["ENDC"] + f"{procent}%", end="\r")
+        time.sleep(sleep)
+    
+    #at the end print the hole line
+    print(Colors["BOLD"] + text, Colors["FAIL"] + f"[{line}]", Colors["ENDC"] + "100%\n\n")
